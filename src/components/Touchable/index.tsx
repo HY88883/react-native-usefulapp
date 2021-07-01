5d7e5eb42472ced6c4f8d4c561e427ba364943ca
Tyrone <xtu1829473134@163.com>
2021-07-01 15:36:30 +0800

    1.1.22

--- src/components/Touchable/index.tsx
+++ src/components/Touchable/index.tsx
@@ -3,9 +3,13 @@ import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
 import _ from 'lodash';
 
 
-const Touchable: React.FC<TouchableOpacityProps> = React.forwardRef(React.memo(({onPress,...rest},ref?:any) => {
+const Touchable: React.FC<TouchableOpacityProps> = React.memo(
+
+    React.forwardRef(({onPress,...rest},ref?:any) => {
     let throttleOnPress = useCallback(_.throttle(onPress, 1000, {leading: true, trailing: false}), [onPress]);
     return  <TouchableOpacity ref={ref} onPress={throttleOnPress} activeOpacity={0.8} {...rest} />
-}) )
+})
+
+)
 
 export default Touchable;
